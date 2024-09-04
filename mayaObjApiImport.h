1400b2dde949ae751b71ef5d97979bd09d8cbb31
Jackey Lea <1768478912@qq.com>
2024-09-04 20:24:34 +0800

    使用文件夹管理文件

--- mayaObjApiImport.h
+++
@@ -1,3 +0,0 @@
-
-// Copyright(c) 2005-2006. All Rights Reserved
-// By Jean-René Bédard (https://github.com/jrbedard/3d-converter)
--- xyzFile.cpp
+++
@@ -1,8 +0,0 @@
-
-// Copyright(c) 2005-2006. All Rights Reserved
-// By Jean-René Bédard (https://github.com/jrbedard/3d-converter)
-
-#include "StdAfx.h"
-
-#include "xyzFile.h"
-
